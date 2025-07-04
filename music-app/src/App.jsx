e5a2f5e55d9910c8a17185b26d3d7881611da37e
shashankanand1409 <shashankanand1409@gmail.com>
2025-07-04 12:06:23 +0530

    set bg and logo

--- music-app/src/App.jsx
+++ music-app/src/App.jsx
@@ -1,35 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import bgImage from './assets/music-sheet-bg.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{
+        minHeight: '100vh',
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className='webpage'>
+        <div className='header'>
+          <div className='button-flex'>
+            <button className="toggle-modes-button">Western</button>
+            <button className="toggle-modes-button">Classical</button> 
+          </div>
+          <div className='song-details'>
+          <h2>Song Details</h2>
+          <p>Details about the selected song will appear here.</p>
+        </div>
+        
+          <div className="header-text-elements"> 
+            <h3>Song title</h3>
+            <h3>Album name</h3>
+            <h3>Artist name</h3>
+            <h3>Tempo</h3>
+            <h3>Time Signature</h3>
+          </div>
+        </div>
+
+        <div className='main-content'>
+            
+        </div>
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
